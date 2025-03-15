777b3e207149a04a77f6cc630fd2ca6303223831
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-03-15 22:19:04 +0530

    setup routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\FileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,7 +15,10 @@ Route::post('/create-user',[UserController::class,'createUser']);
 Route::post('api/fetch-state',[UserController::class,'fetchStates']);
 Route::post('/api/fetch-cities',[UserController::class,'fetchCities']);
 
-
 // add-more
 Route::get('/add-more',[StudentsController::class,'index']);
 Route::post('/add-more',[StudentsController::class,'store']);
+
+Route::get('/add-more-files',[FileController::class,'addForm']); 
+Route::post('/post-data',[FileController::class,'postForm']); 
+Route::get('/list-files',[FileController::class,'listFiles']); 
